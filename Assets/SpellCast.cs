c7c5168993680e8bf83ed9cef9b0c30abeadaa61
maiky95 <maiky95@hotmail.es>
2020-06-09 18:47:16 -0500

    Uploading project

---
+++ Assets/SpellCast.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellCast : MonoBehaviour
+{
+    public Transform firepoint;
+    public GameObject SpellPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+    }
+
+    void Shoot ()
+    {
+        Instantiate(SpellPrefab, firepoint.position, firepoint.rotation);
+    }
+}
