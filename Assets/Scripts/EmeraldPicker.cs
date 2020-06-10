2c0c60af4516e9a7f00e10a424b1e2a0cd57513a
maiky95 <maiky95@hotmail.es>
2020-06-09 19:18:45 -0500

    Refactor Scripts folder

---
+++ Assets/Scripts/EmeraldPicker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EmeraldPicker : MonoBehaviour
+{
+    private float emerald = 0;
+
+    public TextMeshProUGUI textEmerald;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "emerald")
+        {
+            emerald++;
+            textEmerald.text = emerald.ToString();
+
+            Destroy(other.gameObject);
+        }
+
+    }
+
+
+}
