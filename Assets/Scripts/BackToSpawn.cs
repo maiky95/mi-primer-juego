8ea40b909afad1fa99b06b86a17ddad5ad32170b
Gabodp <39098519+Gabodp@users.noreply.github.com>
2020-06-13 00:18:39 +0200

    Organizing folders

---
+++ Assets/Scripts/BackToSpawn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToSpawn : MonoBehaviour
+{
+    private void OnBecameInvisible()
+    {
+
+        transform.parent.transform.position = new Vector3(-15, -2, 0);
+    }
+
+}
